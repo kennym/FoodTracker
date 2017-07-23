346d41706a1ba0076ec1fbee617a2cbe935a6552
Kenny Meyer <kenny@kennymeyer.net>
2017-07-22 21:44:23 -0400

    Finished Food Tracker with Swift

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Kenny Meyer on 7/21/17.
+//  Copyright © 2017 Kenny Meyer. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
